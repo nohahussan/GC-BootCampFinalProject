eb3ac24993befd863cd0172de79dbd764eca80c3
steve webster <steve.webster109@gmail.com>
2019-03-12 10:19:07 -0400

    added login

---
+++ TextToSpeech/Models/Login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TextToSpeech.Models
+{
+    public class Login
+    {
+        [Required]
+        [RegularExpression("^[A-Z]+[A-z]{1,30}$")]
+        public string FirstName { set; get; }
+        [Required]
+        [RegularExpression("^[a-zA-Z]{2,}$")]
+        public string LastName { set; get; }
+        [Required]
+        [RegularExpression(@"^([A-z0-9\.]{5,30})@([A-z]{5,10})\.([a-z]{2,3})$")]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-z0-9]{8,30}$")]
+        public string Password { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-z0-9]{8,30}$")]
+        public string ConfirmPassword { get; set; }
+
+        public Login()
+        {
+            FirstName = "";
+            LastName = "";
+        }
+        public Login(string firstname, string lastname, string email, string password, string confirmpassword)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Email = email;
+            Password = password;
+            ConfirmPassword = confirmpassword;
+        }
+    }
+}
