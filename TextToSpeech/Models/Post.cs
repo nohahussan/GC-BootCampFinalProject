8641209b016d083c28c5413577ee144b24f57a14
bwahler <bmwahler@gmail.com>
2019-03-12 15:16:37 -0400

    Created Database

--- TextToSpeech/Models/Post.cs
+++ TextToSpeech/Models/Post.cs
@@ -9,12 +9,7 @@ using System.Web.Mvc;
 namespace TextToSpeech.Models
 {
     public class Post
-    {
-        //[Key]
-        //public int ID { get; set; }
-        //public string Title { get; set; }
-        //public string File { get; set; }
-        //public string Language { get; set; }
+    {        
         public string Text { get; set; }
 
         public Post()
