f8261c124b791cba034d46b08c905434bbdbc59d
unknown <noha.hussan@gmail.com>
2019-03-18 13:50:17 -0400

    Add Translate API and create TranslateDAL model and TranslatePost model

---
+++ TextToSpeech/Models/TranslatePost.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace translation_API.Models
+{
+    public class TranslatePost
+    {
+
+        public string TranslatedText { get; set; }
+
+
+        public TranslatePost()
+        {
+
+        }
+
+
+
+        public TranslatePost(string APIText)
+        {
+            JObject redditJson = JObject.Parse(APIText);
+
+            JToken post = redditJson["text"];
+
+            TranslatedText = post.ToString();
+
+
+        }
+    }
+}
